2348bea5fca149c20a14ac44e09bd9df60350457
maniraj-mm48 <maniraj.mm48@gmail.com>
2026-03-03 14:38:40 -0500

    Initial Commit

---
+++ DishTime/DishTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishTimeApp.swift
+//  DishTime
+//
+//  Created by Maniraj on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DishTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
